e3117ae199cecbd393e2ca4a93cd72daf0ca55d5
root <11812@holbertonstudents.com>
2026-01-18 19:03:19 +0000

    kjk

---
+++ pagination/3-hypermedia_del_pagination.py
@@ -0,0 +1,27 @@
+from typing import Dict
+
+
+def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+    """Deletion-resilient hypermedia pagination"""
+
+    if index is None:
+        index = 0
+
+    indexed_dataset = self.indexed_dataset()
+    assert index >= 0 and index < len(indexed_dataset)
+
+    data = []
+    current_index = index
+
+    # Collect page_size items, skipping deleted indices
+    while len(data) < page_size and current_index < len(indexed_dataset):
+        if current_index in indexed_dataset:
+            data.append(indexed_dataset[current_index])
+        current_index += 1
+
+    return {
+        "index": index,
+        "next_index": current_index,
+        "page_size": len(data),
+        "data": data
+    }
