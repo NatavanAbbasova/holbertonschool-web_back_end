991596dc6b3b7bd55b2b52b7bf9f47cdc7fe3095
root <11812@holbertonstudents.com>
2026-01-25 19:59:19 +0000

    kjhkj

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -9,20 +9,17 @@ if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
     collection = client.logs.nginx
 
-    # Total logs
     total_logs = collection.count_documents({})
-    print(f"{total_logs} logs")
+    print("{} logs".format(total_logs))
 
-    # Methods
     print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
     for method in methods:
         count = collection.count_documents({"method": method})
-        print(f"\tmethod {method}: {count}")
+        print("\tmethod {}: {}".format(method, count))
 
-    # GET /status
     status_check = collection.count_documents(
         {"method": "GET", "path": "/status"}
     )
-    print(f"{status_check} status check")
+    print("{} status check".format(status_check))
